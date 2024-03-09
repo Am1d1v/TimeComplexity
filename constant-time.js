b51e88aeb8cacf104f6b14cbbd17f018a3110a15
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-09 16:15:59 +0300

    Add files via upload

--- constant-time.js
+++ constant-time.js
@@ -4,5 +4,13 @@ function accessElement(array, index){
   return array[index]
 }
 
-console.log(accessElement([1, 2, 3, 4, 5], 2));
-console.log(accessElement([1, 2, 3,], 2));
+const arrayOne = [1, 2, 3, 4, 5];
+console.time('Access Element 3');
+console.log(accessElement(arrayOne, 2));
+console.timeEnd('Access Element 3')
+
+
+const arrayTwo = Array.from({length: 100}, (_, index) => index + 1);
+console.time('Access Element 90');
+console.log(accessElement(arrayTwo, 89));
+console.timeEnd('Access Element 90')
