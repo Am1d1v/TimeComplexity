21cb82c64aeb43fab643e33f702b3c08f28276f9
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-10 14:35:57 +0300

    Add files via upload
    Quadradic Time Complexity

--- quadradic-time.js
+++ quadradic-time.js
@@ -10,12 +10,25 @@ function sumArray(array){
 
         for (let j = 0; j < array.length; j++) {
             sum2 += array[j];
-            console.log(sum2);
         }
     }
-
 
     return sum + sum2;
 }
 
-console.log(sumArray([1, 2, 3]));
+const arrayOne = [1, 2, 3, 4, 5];
+console.time('Quadradic Time Array 1')
+console.log(sumArray(arrayOne));
+console.timeEnd('Quadradic Time Array 1');
+
+const arrayTwo = Array.from({length: 1000}, (_, index) => index + 1);
+console.time('Quadradic Time Array 2')
+console.log(sumArray(arrayTwo));
+console.timeEnd('Quadradic Time Array 2');
+
+const arrayThree = Array.from({length: 10000}, (_, index) => index + 1);
+console.time('Quadradic Time Array 3')
+console.log(sumArray(arrayThree));
+console.timeEnd('Quadradic Time Array 3');
+
+
