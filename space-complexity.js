08e6b74be5cf3bf8de1c8a87698c99199b0b1dd4
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-10 19:33:36 +0300

    Add files via upload
    Space Complexity

---
+++ space-complexity.js
@@ -0,0 +1,27 @@
+
+
+
+/* 
+  Space Complexity: O(1);
+  Time Complexity: O(1);
+*/
+function add(num1, num2){
+  return num1 + num2;
+}
+
+
+/* 
+  Space Complexity: O(n);
+  Time Complexity: O(n);
+*/
+function createArray(num){
+
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
